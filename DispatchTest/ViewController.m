8494b99b68a0f6455786c71cdf9142ccf845dc9f
Johnny Sparks <johnnyfuchs@gmail.com>
2014-12-10 16:49:51 -0800

    dispatch_once vs creation vs static for strings and dicts

--- DispatchTest/ViewController.m
+++ DispatchTest/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+static NSString *staticString = @"some const value string to referece";
+
 @interface ViewController ()
 
 @end
@@ -16,12 +18,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    NSDictionary *dictionary;
+    NSString *string;
+    NSDate *staticStart = [NSDate date];
+    NSLog(@"start timing");
+    int times = 10000000;
+    while (times--) {
+        static dispatch_once_t onceToken;
+        static NSDictionary *dict;
+        dispatch_once(&onceToken, ^{
+            dict = @{@"somekey":@"someotherlongvalue", @"someotherkey":@"someotherlongvalue", @"onelastkey":@"onelastvalue"};
+        });
+        dictionary = dict;
+    }
+    NSDate *staticEnd = [NSDate date];
+    NSLog(@"finished static dict in %f sec", [staticEnd timeIntervalSinceDate:staticStart]);
+    
+    times = 10000000;
+    while (times--) {
+        dictionary = @{@"somekey":@"someotherlongvalue", @"someotherkey":@"someotherlongvalue", @"onelastkey":@"onelastvalue"};
+    }
+    
+    NSDate *dictEnd = [NSDate date];
+    NSLog(@"finished dict create in %f sec", [dictEnd timeIntervalSinceDate:staticEnd]);
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    times = 10000000;
+    while (times--) {
+        static dispatch_once_t stringOnceToken;
+        static NSString *dispatchString;
+        dispatch_once(&stringOnceToken, ^{
+            dispatchString = @"someotherlongvalue";
+        });
+        string = dispatchString;
+    }
+    NSDate *staticStringEnd = [NSDate date];
+    NSLog(@"finished static string in %f sec", [staticStringEnd timeIntervalSinceDate:dictEnd]);
+    times = 10000000;
+    while (times--) {
+        string = @"someotherlongvalue";
+    }
+    NSDate *stringEnd = [NSDate date];
+    NSLog(@"finished string create in %f sec", [stringEnd timeIntervalSinceDate:staticStringEnd]);
+    times = 10000000;
+    while (times--) {
+        string = staticString;
+    }
+    NSDate *refEnd = [NSDate date];
+    NSLog(@"finished string reference in %f sec", [refEnd timeIntervalSinceDate:stringEnd]);
 }
 
 @end
