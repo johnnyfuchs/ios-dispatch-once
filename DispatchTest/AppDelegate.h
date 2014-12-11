5073824668a7d1f0f81836160895518ec18aef57
Johnny Sparks <johnnyfuchs@gmail.com>
2014-12-10 16:12:56 -0800

    Initial Commit

---
+++ DispatchTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DispatchTest
+//
+//  Created by Johnny Sparks on 12/10/14.
+//  Copyright (c) 2014 Johnny Sparks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DispatchTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DispatchTest
+//
+//  Created by Johnny Sparks on 12/10/14.
+//  Copyright (c) 2014 Johnny Sparks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
